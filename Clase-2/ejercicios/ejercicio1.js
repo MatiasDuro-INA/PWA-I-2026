4615d51f33badf2425a4ba04af3d5c7a6aab188a
Matias <matias.duro@ina.edu.ar>
2026-03-26 20:56:57 -0300

    ejercicios clase 2

--- Clase-2/ejercicios/ejercicio1.js
+++ Clase-2/ejercicios/ejercicio1.js
@@ -1,2 +1,29 @@
-let numero = 7
-console.log(numero != "7");
+// en una funcion, separa y devolve un objeto, con los hombres y mujeres separados
+
+
+const separarUsuarios = (usuarios) => {
+
+    // inicializar las variables de hombre y mujer
+    
+
+    // for of
+
+}
+
+
+const usuarios = [
+    { nombre: "Ana", sexo: "F" },
+    { nombre: "Facu", sexo: "M" },
+    { nombre: "Sofia", sexo: "F" },
+    { nombre: "Manuel", sexo: "M" },
+    { nombre: "Martina", sexo: "F" },
+]
+
+const resultado = separarUsuarios(usuarios)
+
+console.log(resultado);
+// respuesta
+// {
+//     mujeres: [{},{}]
+//     hombres: [{},{}]
+// }
