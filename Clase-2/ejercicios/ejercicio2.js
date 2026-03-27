1c2b89c23a817f3a2c7b86eab9c6ef32857e7c11
Matias <matias.duro@ina.edu.ar>
2026-03-26 21:16:15 -0300

    clase 2 ejercicios

---
+++ Clase-2/ejercicios/ejercicio2.js
@@ -0,0 +1,24 @@
+const inventario = [
+    { nombre: "Teclado", cantidad: 10 },
+    { nombre: "Mouse", cantidad: 0 },
+    { nombre: "Monitor", cantidad: 7 },
+    { nombre: "Cable HDMI", cantidad: 0 },
+    { nombre: "Auriculares", cantidad: 4 },
+    { nombre: "Webcam", cantidad: 0 },
+]
+
+
+// separar los productos en stock vs agotados
+
+//tips:
+// for of
+// condicionales
+// push
+
+// crea una funcion y retorna
+// {
+    // enStock: [],
+    // agotados: []
+// }
+
+
